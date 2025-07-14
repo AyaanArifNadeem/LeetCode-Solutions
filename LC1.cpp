a9bef25985d447387fa08b299f5a0cd7016a1422
Ayaan Arif Nadeem <socialayaan@gmail.com>
2025-07-14 13:30:24 +0500

    Stock Buy and Sell

---
+++ LC1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Brute-Force
+vector<int> twoSum(vector<int> &nums, int target){
+    vector<int> ans;
+
+    for (int i = 0; i < nums.size(); i++){
+        if (ans.size() == 2){break;}
+        for (int j = 0; j < nums.size(); j++){
+            if (ans.size() == 2){break;}
+            if (i == j){continue;}
+            if (nums.at(i) + nums.at(j) == target){
+                ans.push_back(i);
+                ans.push_back(j);
+            }
+        }
+    }
+    return ans;
+}
