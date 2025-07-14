8a68b85920e77184215eb738402a89177ec04a17
Ayaan Arif Nadeem <socialayaan@gmail.com>
2025-07-14 13:31:35 +0500

    Target Sum-Brute Force

--- LC1.cpp
+++ LC1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 //Brute-Force
 vector<int> twoSum(vector<int> &nums, int target){
     vector<int> ans;
-
+ 
     for (int i = 0; i < nums.size(); i++){
         if (ans.size() == 2){break;}
         for (int j = 0; j < nums.size(); j++){
